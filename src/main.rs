d420c3fc12768b3071e67ad23f52fbe19331acb7
jaennil <jaennil@yandex.ru>
2024-07-16 13:19:09 +0300

    feat: binary

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-fn main() {}
+use std::{thread::sleep, time::Duration};
 
-#[allow(dead_code)]
+use arboard::Clipboard;
+
+fn main() {
+    let mut clipboard = Clipboard::new().unwrap();
+    let input = clipboard.get_text().unwrap();
+    let translated = translate(&input);
+    clipboard.set_text(translated).unwrap();
+    sleep(Duration::from_secs(3));
+}
+
 fn translate(text: &str) -> String {
     text.split_whitespace()
         .map(|word| match word.chars().count() {
@@ -16,77 +25,79 @@ fn translate(text: &str) -> String {
 }
 
 fn translate_word(word: &str) -> String {
-    let mut res = word.to_string();
+    let mut result = word.to_string();
     let cur_char: &char;
     if word.chars().count() > 2 {
-        res = res.replace("его", "аго");
-        res = res.replace("ого", "аго");
-        res = res.replace("ая", "ыя");
-        res = res.replace("её", "ея");
-        res = res.replace("ее", "ея");
-        res = res.replace("ие", "iя");
-        res = res.replace("иё", "iя");
-        res = res.replace("ия", "iя");
+        result = result.replace("его", "аго");
+        result = result.replace("ого", "аго");
+        result = result.replace("ая", "ыя");
+        result = result.replace("её", "ея");
+        result = result.replace("ее", "ея");
+        result = result.replace("ие", "iя");
+        result = result.replace("иё", "iя");
+        result = result.replace("ия", "iя");
 
-        res = res.replace("ии", "iи");
-        res = res.replace("ий", "iй");
+        result = result.replace("ии", "iи");
+        result = result.replace("ий", "iй");
 
-        res = res.replace("иу", "iу");
-        res = res.replace("иы", "iы");
-        res = res.replace("иа", "iа");
-        res = res.replace("ио", "iо");
-        res = res.replace("иэ", "iэ");
-        res = res.replace("ию", "iю");
+        result = result.replace("иу", "iу");
+        result = result.replace("иы", "iы");
+        result = result.replace("иа", "iа");
+        result = result.replace("ио", "iо");
+        result = result.replace("иэ", "iэ");
+        result = result.replace("ию", "iю");
 
-        res = res.replace("бе", "бѣ");
-        res = res.replace("ве", "вѣ");
-        res = res.replace("вё", "вѣ");
-        res = res.replace("де", "дѣ");
-        res = res.replace("же", "жѣ");
-        res = res.replace("зе", "зѣ");
-        res = res.replace("ке", "кѣ");
-        res = res.replace("ле", "лѣ");
-        res = res.replace("не", "нѣ");
-        res = res.replace("мё", "мѣ");
-        res = res.replace("ме", "мѣ");
-        res = res.replace("пе", "пѣ");
-        res = res.replace("рё", "рѣ");
-        res = res.replace("ре", "рѣ");
-        res = res.replace("се", "сѣ");
-        res = res.replace("те", "тѣ");
-        res = res.replace("тё", "тѣ");
-        res = res.replace("це", "цѣ");
+        result = result.replace("бе", "бѣ");
+        result = result.replace("ве", "вѣ");
+        result = result.replace("вё", "вѣ");
+        result = result.replace("де", "дѣ");
+        result = result.replace("же", "жѣ");
+        result = result.replace("зе", "зѣ");
+        result = result.replace("ке", "кѣ");
+        result = result.replace("ле", "лѣ");
+        result = result.replace("не", "нѣ");
+        result = result.replace("мё", "мѣ");
+        result = result.replace("ме", "мѣ");
+        result = result.replace("пе", "пѣ");
+        result = result.replace("рё", "рѣ");
+        result = result.replace("ре", "рѣ");
+        result = result.replace("се", "сѣ");
+        result = result.replace("те", "тѣ");
+        result = result.replace("тё", "тѣ");
+        result = result.replace("це", "цѣ");
 
-        res = res.replace("Бе", "Бѣ");
-        res = res.replace("Ве", "Вѣ");
-        res = res.replace("вё", "Вѣ");
-        res = res.replace("Де", "Дѣ");
-        res = res.replace("Же", "Жѣ");
-        res = res.replace("Зе", "Зѣ");
-        res = res.replace("Ке", "Кѣ");
-        res = res.replace("Ле", "Лѣ");
-        res = res.replace("Не", "Нѣ");
-        res = res.replace("Ме", "Мѣ");
-        res = res.replace("Мё", "Мѣ");
-        res = res.replace("Пе", "Пѣ");
-        res = res.replace("Ре", "Рѣ");
-        res = res.replace("Рё", "Рѣ");
-        res = res.replace("Се", "Сѣ");
-        res = res.replace("Те", "тѣ");
-        res = res.replace("Тё", "тѣ");
-        res = res.replace("Це", "Цѣ");
+        result = result.replace("Бе", "Бѣ");
+        result = result.replace("Ве", "Вѣ");
+        result = result.replace("вё", "Вѣ");
+        result = result.replace("Де", "Дѣ");
+        result = result.replace("Же", "Жѣ");
+        result = result.replace("Зе", "Зѣ");
+        result = result.replace("Ке", "Кѣ");
+        result = result.replace("Ле", "Лѣ");
+        result = result.replace("Не", "Нѣ");
+        result = result.replace("Ме", "Мѣ");
+        result = result.replace("Мё", "Мѣ");
+        result = result.replace("Пе", "Пѣ");
+        result = result.replace("Ре", "Рѣ");
+        result = result.replace("Рё", "Рѣ");
+        result = result.replace("Се", "Сѣ");
+        result = result.replace("Те", "тѣ");
+        result = result.replace("Тё", "тѣ");
+        result = result.replace("Це", "Цѣ");
 
-        res = res.replace("Ед", "ѣд");
-        res = res.replace("ед", "Ѣд");
+        result = result.replace("Ед", "ѣд");
+        result = result.replace("ед", "Ѣд");
 
-        res = res.replace("ри", "рi");
-        res = res.replace("ни", "нi");
+        result = result.replace("ри", "рi");
+        result = result.replace("ни", "нi");
     }
 
-    let chars = res.chars().collect::<Vec<_>>();
-    let last_char = chars.last().unwrap();
-
+    let chars = result.chars().collect::<Vec<_>>();
+    let last_char = chars
+        .last()
+        .expect("impossible last char to be empty as i check chars amount to be > 2");
     let mut flag = false;
+
     if *last_char == '.'
         || *last_char == ','
         || *last_char == ';'
@@ -98,7 +109,7 @@ fn translate_word(word: &str) -> String {
         || *last_char == '"'
         || *last_char == '»'
     {
-        res = res[..res.len() - 1].to_string();
+        result.pop();
         cur_char = chars.get(chars.len() - 2).unwrap();
     } else {
         cur_char = last_char;
@@ -126,17 +137,17 @@ fn translate_word(word: &str) -> String {
         || *cur_char == 'ш'
         || *cur_char == 'щ'
     {
-        res += "ъ";
+        result += "ъ";
     } else if *cur_char == 'ы' {
-        res = res[..res.len() - 1].to_string();
-        res.push('ъ');
+        result.pop();
+        result.push('ъ');
     }
 
     if !flag {
-        res.push(*last_char);
+        result.push(*last_char);
     }
 
-    return res;
+    return result;
 }
 
 #[cfg(test)]
@@ -155,6 +166,14 @@ mod tests {
     fn test2() {
         let input = "В итоге";
         let expected = "Въ итоге";
+        let actual = translate(input);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test3() {
+        let input = r#"С помощью этого конвертера вы сможете оформить текст по старым правилам, точно он был написан в середине XIX — начале XX века, до революции и до так называемой "орфографической реформы", упростившей правила написания текстов. Если сегодня ретро-орфографический стиль — это просто оригинальный стилистический эффект, то в прежнее время все тексты в Царской России писались именно таким образом в соответствии с официально принятыми орфографическими правилами. Это было обязательное требование, особенно для деловых документов, писем, литературных, художественных произведений, церковнославянских текстов."#;
+        let expected = r#"Съ помощью этаго конвѣртѣра вы сможѣтѣ оформить тѣкстъ по старымъ правиламъ, точно он былъ написанъ въ сѣрѣдинѣ XIX — началѣ XX вѣка, до рѣволюцiи и до такъ называемой "орфографической рѣформы", упростившей правила написанiя тѣкстовъ. Если сагодня рѣтро-орфографическiй стиль — это просто орiгинальный стилистическiй эффектъ, то въ прѣжнѣя врѣмя всѣ тѣкстъ въ Царской Россiи писались имѣнно такимъ образомъ въ соотвѣтствiи съ офицiально прiнятыми орфографическими правилами. Это было обязатѣльное трѣбованiя, особѣнно для дѣловыхъ докумѣнтовъ, писѣмъ, литѣратурныхъ, художѣствѣнныхъ произвѣдѣнiй, цѣрковнославянскихъ тѣкстовъ."#;
         let actual = translate(input);
         assert_eq!(expected, actual);
     }
